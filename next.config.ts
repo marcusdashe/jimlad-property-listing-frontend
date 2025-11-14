1e1f4bf67b01e35d8a0812b0ef890f1c305ac60e
Marcus Dashe <marcusdashe.developer@gmail.com>
2025-11-14 12:11:12 +0000

    fix this
    Error: Invalid src prop (https://images.nigeriapropertycentre.

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.nigeriapropertycentre.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
