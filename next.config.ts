46cc8afd6980c10e091e5fcf3b4a040245adb421
Marcus Dashe <marcusdashe.developer@gmail.com>
2025-11-14 12:28:31 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch. Error sou

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3000/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
